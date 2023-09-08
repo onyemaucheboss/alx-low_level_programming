f0adcd657785a1fe82b9bd72e3c0b1054543f1a9
onyemaucheboss <obikaonuonyemauche@gmail.com>
2023-09-08 16:33:17 -0700

    this is becoming cool am still awake by this time of the night

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using the printf function
- * Return: 0 (success)
+ * main - Entry point of the program
+ *
+ * Description: This function to print the text
+ * "with proper gramm
+ *
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
