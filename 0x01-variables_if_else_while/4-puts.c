6b443efcaea77a5b8b141b3cbc5fb638e8ae5b09
onyemaucheboss <obikaonuonyemauche@gmail.com>
2023-09-08 15:54:33 -0700

    4-puts.c commit

---
+++ 0x01-variables_if_else_while/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function uses the `puts` function to print the text
+ * "Programming is like building a multilingual puzzle" followed by a new line.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
