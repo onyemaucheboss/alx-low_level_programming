d21fea8aadca0d26054711bd26177b1360a12bc3
onyemaucheboss <obikaonuonyemauche@gmail.com>
2023-09-08 22:38:29 -0700

    101-quote.c commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a message to the standard error
+ * using the write system call.
+ *
+ * Return: 1 (Error)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
