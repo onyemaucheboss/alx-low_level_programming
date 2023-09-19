26cd7c2c023a6a1d0ddebf6f88f2635c1f7bb72d
onyemaucheboss <obikaonuonyemauche@gmail.com>
2023-09-18 18:08:40 -0700

    6-puts2.c commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string.
+ * @str: The string to be printed.
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2; /* Increment by 2 to skip every other character */
+	}
+
+	_putchar('\n'); /* Print a new line character */
+}
