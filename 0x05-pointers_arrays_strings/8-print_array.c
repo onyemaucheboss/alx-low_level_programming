a17c2f9dd53f9854e3f92c3c2c910ac3e206ebf8
onyemaucheboss <obikaonuonyemauche@gmail.com>
2023-09-19 05:47:40 -0700

    8-print_array.c commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,24 +2,23 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints n elements of an array of integers, followed by a new line.
+ * print_array - Prints n elements of an e.
  * @a: The array of integers.
  * @n: The number of elements to be printed.
  */
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", a[i]); // Print the current element
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]); /* Print the current element */
 
-        if (i != n - 1) // Check if it's not the last element
-        {
-            printf(", "); // Print a comma and space
-        }
-    }
+		if (i != n - 1) /* Check if it's not the last element */
+		{
+			printf(", "); /* Print a comma and space */
+		}
+	}
 
-    printf("\n"); // Print a new line character
+	printf("\n"); /* Print a new line character */
 }
-
