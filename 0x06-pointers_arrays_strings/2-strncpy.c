ddb61f4e953d566bb220498fca1dfb05499aed02
onyemaucheboss <obikaonuonyemauche@gmail.com>
2023-09-21 20:06:17 -0700

    2-strncpy.c commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies at most n bytes from source to destination
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The maximum number of bytes to copy from src
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
